722b733ec37315a7b92a285347ee4d93d4c13c7b
VIP666-star <981613301@qq.com>
2026-08-19 08:14:05 +0800

    创建WarriorGameplayTag

---
+++ Source/Warrior/Private/WarriorGameplayTag.cpp
@@ -0,0 +1,13 @@
+// A Gas Demo
+
+
+#include "WarriorGameplayTag.h"
+
+namespace WarriorGameplayTag
+{
+	/** Input Tag **/
+	
+	UE_DEFINE_GAMEPLAY_TAG(Input_Move, "Input.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Look, "Input.Look");
+	
+}
---
+++ Source/Warrior/Public/WarriorGameplayTag.h
@@ -0,0 +1,14 @@
+// A Gas Demo
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTag
+{
+	/** Input Tag **/
+	
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Move);
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Look);
+	
+}
